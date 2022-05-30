b89c143704162dfc22ffbf0a40dc9e59b4a2d142
renukaveer12 <renukaveer27@gmail.com>
2022-05-30 14:26:15 +0530

    Initial Comment

---
+++ Unsecurity/src/main/java/com/example/Unsecurity/UnsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsecurityApplication.class, args);
+	}
+
+}
