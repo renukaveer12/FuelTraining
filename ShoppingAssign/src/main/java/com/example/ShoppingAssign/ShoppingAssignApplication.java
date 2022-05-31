27c859de93ac72e1c93eb9b1cf7d62085dae1bb9
renukaveer12 <renukaveer27@gmail.com>
2022-05-31 12:53:59 +0530

    Initial Comment

---
+++ ShoppingAssign/src/main/java/com/example/ShoppingAssign/ShoppingAssignApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAssignApplication.class, args);
+	}
+
+}
